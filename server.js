41a9789c8fb66ddcf7e9927a90f6a62801fe1e5a
JoaoVitor83 <vititor666demon@gmail.com>
2022-10-19 23:32:13 -0300

    Alterações no FAQ

--- server.js
+++ server.js
@@ -1,29 +1,39 @@
-const { Console } = require('console');
-const path = require('path');
-const express = require('express');
+const { Socket } = require("dgram");
+const express = require("express");
+const path = require("path");
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
 
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'public'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "public"));
+app.engine("html", require("ejs").renderFile); // Usado para renderizar um arquivo .html
+app.set("view engine", "html");
 
-server.listen(3000, function(){
-
-    console.log('Conexão feita na porta 3000');
-    
+app.use("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.get('/', (req, res) =>{
+let messages = [];
 
-    res.sendFile(__dirname+'/index.html');
+io.on("connection", (socket) => {
+  console.log("Um usuário se conectou");
+
+  socket.on("chat message", (msg) => {
+    socket.on("Nome", (nome) => {
+      socket.broadcast.emit(nome, msg);
+      io.emit(nome, msg);
+      console.log(nome + ": " + msg);
+    });
+  });
+
+  socket.on("mensagem", (data) => {
+    messages.push(data);
+    socket.broadcast.emit("mensagemRecebida", data);
+  });
 });
 
-io.on('connection', (socket) =>{
-
-    console.log('Nova conexão ', socket.id);
-
+server.listen(3000, () => {
+  console.log("Servidor conectado na porta 3000");
 });
